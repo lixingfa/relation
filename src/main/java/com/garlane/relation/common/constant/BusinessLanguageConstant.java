6b94c4267bc388a704c44ea9dd66c4e549fa556a
李兴发 <346302606@qq.com>
2018-06-10 22:09:10 +0800

    新修改

---
+++ src/main/java/com/garlane/relation/common/constant/BusinessLanguageConstant.java
@@ -0,0 +1,8 @@
+package com.garlane.relation.common.constant;
+
+public class BusinessLanguageConstant {
+	public static final String SELECT = "查询";
+	public static final String SINGLE = "单个";
+	public static final String LIST = "列表";
+	//未读已读总数修改密码退出登录返回权限结束
+}
