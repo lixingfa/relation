73a9d1c38708a332b56289154055811cfa265b30
李兴发 <lixingfa@minstone.com.cn>
2018-07-27 15:16:54 +0800

    添加JSON格式化编码，便于阅读

--- src/main/java/com/garlane/relation/analyze/service/impl/FileAnalyzeServiceImpl.java
+++ src/main/java/com/garlane/relation/analyze/service/impl/FileAnalyzeServiceImpl.java
@@ -117,6 +117,8 @@ public class FileAnalyzeServiceImpl implements FileAnalyzeService {
 					path = htmlModel.getPath();
 					path = path.substring(path.indexOf("\\jsp"));
 					System.out.println(path);
+					//格式化，便于阅读
+					modelString = getJsonFormat(modelString);
 					//将对象写入文本，对比提前结果
 					FileUtils.writeTxtFile(modelString, "E:/htmlModels/" + path + ".txt");
 				}				
@@ -408,4 +410,51 @@ public class FileAnalyzeServiceImpl implements FileAnalyzeService {
 		}
 		return url;
 	}
+	
+	
+	/*****************************************************/
+	private String getJsonFormat(String s){
+		int level = 0;
+        //存放格式化的json字符串
+        StringBuffer jsonForMatStr = new StringBuffer();
+        for(int index=0;index<s.length();index++)//将字符串中的字符逐个按行输出
+        {
+            //获取s中的每个字符
+            char c = s.charAt(index);
+            //level大于0并且jsonForMatStr中的最后一个字符为\n,jsonForMatStr加入\t
+            if (level > 0 && '\n' == jsonForMatStr.charAt(jsonForMatStr.length() - 1)) {
+                jsonForMatStr.append(getLevelStr(level));
+            }
+            //遇到"{"和"["要增加空格和换行，遇到"}"和"]"要减少空格，以对应，遇到","要换行
+            switch (c) {
+            case '{':
+            case '[':
+                jsonForMatStr.append(c + "\n");
+                level++;
+                break;
+            case ',':
+                jsonForMatStr.append(c + "\n");             
+                break;
+            case '}':
+            case ']':
+                jsonForMatStr.append("\n");
+                level--;
+                jsonForMatStr.append(getLevelStr(level));
+                jsonForMatStr.append(c);
+                break;
+            default:
+                jsonForMatStr.append(c);
+                break;
+            }
+        }        
+        return jsonForMatStr.toString();
+	}
+	
+	private static String getLevelStr(int level) {
+        StringBuffer levelStr = new StringBuffer();
+        for (int levelI = 0; levelI < level; levelI++) {
+            levelStr.append("\t");
+        }
+        return levelStr.toString();
+    }
 }
