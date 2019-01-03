69a233a7bc1787e0a9b9087a8834cc0471ab774c
李兴发 <lixingfa@minstone.com.cn>
2019-01-03 15:22:32 +0800

    实现get/set

--- src/main/java/com/garlane/relation/analyze/model/logic/Class.java
+++ src/main/java/com/garlane/relation/analyze/model/logic/Class.java
@@ -8,6 +8,7 @@ public class Class {
 	private String className;
 	/**类的属性*/
 	private Set<ClassProperty> properties = new HashSet<ClassProperty>();
+	//属于哪个页面的，可以看其属于哪个htmlModel
 	/********************************************/
 	public Class(String className){
 		this.className = className;
@@ -30,13 +31,26 @@ public class Class {
 	
 	public String toString(){
 		StringBuffer s = new StringBuffer("public class " + className + "{");
+		StringBuffer getSetBuffer = new StringBuffer();
 		for (ClassProperty property : properties) {
 			if (property.getTitle() != null) {
 				s.append("/**").append(property.getTitle()).append("*/");
 			}
 			s.append("private ").append(property.getType()).append(" ").append(property.getPropertyName()).append(";\n");
+			
+			getSetBuffer.append("\npublic ").append(property.getType()).append(" get").append(upFirst(property.getPropertyName())).append("(){")
+			.append("return ").append(property.getPropertyName()).append(";").append("}");
+			
+			getSetBuffer.append("\npublic void ").append(" set").append(upFirst(property.getPropertyName()))
+			.append("(").append(property.getType()).append(" ").append(property.getPropertyName()).append("){")
+			.append("this.").append(property.getPropertyName()).append(" = ").append(property.getPropertyName()).append(";").append("}");
 		}
-		return s.append("}").toString();
-		
+		return s.append("\n").append(getSetBuffer).append("}").toString();		
 	}
+	
+	private String upFirst(String s){
+		String first = s.substring(0, 1);
+		return first.toUpperCase() + s.substring(1);
+	}
+	
 }
